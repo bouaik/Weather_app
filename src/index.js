df774c9e6068d31e1eee9de3c4e863f8ed11265e
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-19 23:08:18 +0100

    show alert if input is empty

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import 'bootstrap'
 import './styles/main.scss'
 
-console.log('hello world')
+
+
+(function () {
+
+    const form = document.getElementById('wheatherForm')
+    const cityInput = document.getElementById('cityInput')
+    const feedback = document.querySelector('.feedback')
+    const loading = document.querySelector('.spinner-border')
+
+
+    function showFeedback(text) {
+        feedback.classList.add('showItem')
+        feedback.innerHTML = `<p>${text}</p>`
+
+        setTimeout(()=> {
+            feedback.classList.remove('showItem')
+        }, 3000)
+    }
+
+})(); 
