d6623e732472b54b0755409832d20e5f9534d3fa
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-19 23:23:00 +0100

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,48 +1,47 @@
-import 'bootstrap'
-import './styles/main.scss'
+import 'bootstrap';
+import './styles/main.scss';
 
-import AjaxWheather from './js/ajaxfetch'
-import Display from './js/display'
+import AjaxWheather from './js/ajaxfetch';
+import Display from './js/display';
 
 
-(function () {
+(function () { // eslint-disable-line
+  const form = document.getElementById('wheatherForm');
+  const cityInput = document.getElementById('cityInput');
+  const feedback = document.querySelector('.feedback');
+  const loading = document.querySelector('.spinner-border');
 
-    const form = document.getElementById('wheatherForm')
-    const cityInput = document.getElementById('cityInput')
-    const feedback = document.querySelector('.feedback')
-    const loading = document.querySelector('.spinner-border')
+  const ajax = new AjaxWheather();
+  const display = new Display();
 
-    const ajax = new AjaxWheather()
-    const display = new Display()
 
-    form.addEventListener('submit', e => {
-        e.preventDefault()
+  function showFeedback(text) {
+    feedback.classList.add('showItem');
+    feedback.innerHTML = `<p>${text}</p>`;
 
-        const city = cityInput.value 
+    setTimeout(() => {
+      feedback.classList.remove('showItem');
+    }, 3000);
+  }
 
-        if (city === '') {
-            showFeedback('city value cannot be empty')
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    if (city === '') {
+      showFeedback('city value cannot be empty');
+    } else {
+      loading.classList.add('showItem');
+      ajax.getWheather(city).then(data => {
+        if (data.message === 'city not found') {
+          showFeedback('city with such name cannot be found');
         } else {
-            loading.classList.add('showItem')
-            ajax.getWheather(city).then(data => {
-                if(data.message === 'city not found') {
-                    showFeedback('city with such name cannot be found')
-                } else {
-                    loading.classList.remove('showItem')
-                    display.showWeather(data)
-                    cityInput.value = ''
-                }
-            })
+          loading.classList.remove('showItem');
+          display.showWeather(data);
+          cityInput.value = '';
         }
-    })
-
-    function showFeedback(text) {
-        feedback.classList.add('showItem')
-        feedback.innerHTML = `<p>${text}</p>`
-
-        setTimeout(()=> {
-            feedback.classList.remove('showItem')
-        }, 3000)
+      });
     }
-
-})(); 
+  });
+}());
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
     contentBase: path.join(process.cwd(), 'dist'),
     compress: true,
-    port: 8050
+    port: 8050,
   },
   module: {
     rules: [{
-     test: /\.scss$/,
-     use: [
-    "style-loader", // creates style nodes from JS strings
-    "css-loader", // translates CSS into CommonJS
-    "sass-loader" // compiles Sass to CSS, using Node Sass by default
-     ]
-    }]
-  }
+      test: /\.scss$/,
+      use: [
+        'style-loader', // creates style nodes from JS strings
+        'css-loader', // translates CSS into CommonJS
+        'sass-loader', // compiles Sass to CSS, using Node Sass by default
+      ],
+    }],
+  },
 };
