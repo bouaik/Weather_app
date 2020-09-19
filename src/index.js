eb3725acd21698a4207d3611b74b08a92b434c86
Lhoussaine <bouaik.lhou@gmail.com>
2020-09-19 23:10:48 +0100

    Get data fom the API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap'
 import './styles/main.scss'
+import AjaxWheather from './js/ajaxfetch'
 
 
 
@@ -10,6 +11,27 @@ import './styles/main.scss'
     const feedback = document.querySelector('.feedback')
     const loading = document.querySelector('.spinner-border')
 
+    const ajax = new AjaxWheather()
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+
+        const city = cityInput.value 
+
+        if (city === '') {
+            showFeedback('city value cannot be empty')
+        } else {
+            loading.classList.add('showItem')
+            ajax.getWheather(city).then(data => {
+                if(data.message === 'city not found') {
+                    showFeedback('city with such name cannot be found')
+                } else {
+                    loading.classList.remove('showItem')
+                    cityInput.value = ''
+                }
+            })
+        }
+    })
 
     function showFeedback(text) {
         feedback.classList.add('showItem')
